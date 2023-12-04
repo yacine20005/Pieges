8cd96ab87a58ef7b963d9466122bef07515d6d39
yacine20005 <yacine200005@gmail.com>
2023-12-04 15:22:51 +0100

    Remaniment des différentes structures de données et des fonctions gérant les tirettes et plateau

--- yacine.py
+++ yacine.py
@@ -1,9 +1,20 @@
 def creation_grille():
     lst = []
-    for k in range(13):
+    for k in range(7):
         lst2 = []
-        for x in range(13):
+        for x in range(7):
             lst2.append(0)
+        lst.append(lst2)
+    return lst
+
+def poser_bille(grille,x,y):
+    grille[y][x] = 1
+    return grille
+    
+def creation_tirette():
+    lst = []
+    lst2 = [1, 0, 0, 1, 0, 0, 1]
+    for i in range(7):
         lst.append(lst2)
     return lst
 
@@ -12,22 +23,17 @@ def deplacer_tirets_horizontal(grille, tiret, direction):
         if direction < 0:
             if grille[tiret][x] == 1:
                 grille[tiret][x] = 0
-                grille[tiret][x - direction] = 1
+                grille[tiret][x + direction] = 1
         if direction > 0:
             x = len(grille[tiret]) - x
             if grille[tiret][x] == 1:
                 grille[tiret][x] = 0
                 grille[tiret][x + direction] = 1
 
-def deplacer_tirets_horizontal_merde(grille, tiret, direction):
-    for x in range(7):
-        if x - direction > 0:
-                if direction < 0:
-                    grille[tiret][x-direction] = grille[tiret][x]
-                elif direction > 0:
-                    grille[tiret][x+direction] = grille[tiret][x]
-
 plateau = creation_grille()
-tirette_verticale = creation_grille()
-tirette_horizontale = creation_grille()
-print(plateau)
+tirette_verticale = creation_tirette()
+tirette_horizontale = creation_tirette()
+#print(plateau)
+#print(tirette_verticale)
+#tirette_horizontale = deplacer_tirets_horizontal(tirette_horizontale, 1, 1)
+#print(tirette_horizontale)
