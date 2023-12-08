0214a08a20e0f1575d7982e1cf9086b546aed163
SkYY6809 <vous.la.connaissez@gmail.com>
2023-12-08 12:43:58 +0100

    GG

--- Liam.py
+++ Liam.py
@@ -2,24 +2,45 @@ import random
 import doctest
 import sys
 
-def creation_grille():
+def creation_grille_B():
     lst = []
     for k in range(7):
         lst2 = []
         for x in range(7):
-            lst2.append(False)
+            chance = random.randint(1, 2)
+            if chance == 1:
+                lst2.append(False)
+            else:
+                lst2.append(True)
         lst.append(lst2)
     return lst
 
-def creation_tirette():
+def creation_grille_H():
     lst = []
-    for i in range():
-        x = randint(0,1)
-        lst2.append(x])
-    lst2 = [True,False,False,True,False,False,True]
-        for k in range (7):
-            lst.append(lst2)
-        return lst
+    for k in range(7):
+        lst2 = []
+        for x in range(7):
+            chance = random.randint(1, 2)
+            if chance == 1:
+                lst2.append(0)
+            else:
+                lst2.append(1)
+        lst.append(lst2)
+    return lst
+
+def creation_grille_V():
+    lst = []
+    for k in range(7):
+        lst2 = []
+        for x in range(7):
+            chance = random.randint(1, 2)
+            if chance == 1:
+                lst2.append(0)
+            else:
+                lst2.append(2)
+        lst.append(lst2)
+    return lst
+
 
 def deplacer_droite(tirette):
     last_val = tirette.pop()
