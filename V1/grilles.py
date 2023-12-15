642e80a47b50458ec0c7ce2c74ff0d78653a79d3
yacine20005 <yacine200005@gmail.com>
2023-12-15 12:01:44 +0100

    Division de la V1 en plusieurs parties

--- V1/grilles.py
+++ V1/grilles.py
@@ -1,10 +1,4 @@
 import random
-import doctest
-import sys
-
-""" Variable global """
-nb_coup = 0
-
 
 def creation_grille_B():
     """
@@ -121,35 +115,10 @@ def deplacer_haut(grille, colonne):
     print(f"Déplacement vers le haut de la colonne {colonne}")
 
 
-def bille_en_vie(grille_B, grille_H, grille_V):
-    """
-    Parcourt l'ensemble de la grille pour faire tomber et donc disparaître les billes du plateau ayant un trou en dessous d'eux
-
-    Args:
-        grille_B (list): Tableau en 2 dimensions représentant la couche des billes
-        grille_H (list): Tableau en 2 dimensions représentant les tirettes horizontaux
-        grille_V (list): Tableau en 2 dimensions représentant les tirettes verticaux
-    """
-    for y in range(len(grille_B)):
-        for x in range(len(grille_B[0])):
-            if grille_H[y][x] + grille_V[y][x] == 0:
-                grille_B[y][x] = False
-
-def victoire(grille_B):
-    """
-    Parcourt l'ensemble de la 
-
-    Args:
-        grille_B (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    for y in range(len(grille_B) - 1):
-        for x in range(len(grille_B[0]) - 1):
-            if grille_B[y][x] == True:
-                return False
-    return True
+def affichage_grille(grille):
+    for x in grille:
+        print(x)
+    print("")
 
 def comparaison(PL,TV,TH):
     for y in range(len(PL)):
@@ -165,72 +134,3 @@ def fusion(TV, TH):
             LstVide2.append(TV[y][x] + TH[y][x])
         LstVide.append(LstVide2)
     return LstVide
-
-def compteur_de_coup(nb_coup):
-    nb_coup += 1
-    return nb_coup
-
-def affichage_grille(grille):
-    for x in grille:
-        print(x)
-    print("")
-
-#Boucle de gameplay
-
-V = creation_grille_V()
-H = creation_grille_H()
-B = creation_grille_B()
-nb_coup = 0
-victoire(B)
-
-print("Bienvenue dans le jeu Pieges !")
-print("Les tirettes horizontaux sont composes de 0 et de 1")
-print("Tandis que les tirettes verticaux sont composes de 0 et de 2")
-
-
-while victoire(B) is False:
-
-    tirette = None
-    mouv = ""
-    direction = ""
-    plateau = fusion(V, H)
-    print("")
-    affichage_grille(plateau)
-    affichage_grille(B)
-    while direction not in ("H", "V", "STOP"):
-        direction = input("Voulez-vous deplacer les tirettes horizontaux ou verticaux ? (H ou V ou STOP) : ")
-
-    if direction == "V":
-        while tirette not in range(len(B)) :
-            tirette = (input("Quelle tirette voulez-vous deplacer ? (0 - 6) : "))
-            if tirette.isdigit():
-                tirette = int(tirette)
-        while mouv not in ("H", "B"):
-            mouv = input("Dans quelle direction ? (H ou B) : ")
-        if mouv == "B":
-            deplacer_bas(V, tirette)
-        elif mouv == "H":
-            deplacer_haut(V, tirette)
-        bille_en_vie(B, H, V)
-        nb_coup += 1
-        print("")
-        print("Vous avez deplacer des tirettes", nb_coup, "fois")
-
-    elif direction == "H":
-        while tirette not in range(len((B)[0])):
-            tirette = (input("Quelle tirette voulez-vous deplacer ? (0 - 6) : "))
-            if tirette.isdigit():
-                tirette = int(tirette)
-        while mouv not in ("G", "D"):
-            mouv = input("Dans quelle direction ? (D ou G) : ")
-        if mouv == "G":
-            deplacer_gauche(H, tirette)
-        elif mouv == "D":
-            deplacer_droite(H, tirette)
-        bille_en_vie(B, H, V)
-        nb_coup += 1
-        print("")
-        print("Vous avez deplacer des tirettes", nb_coup, "fois")
-
-    elif direction == "STOP":
-        break
---
+++ V1/regles.py
@@ -0,0 +1,29 @@
+def bille_en_vie(grille_B, grille_H, grille_V):
+    """
+    Parcourt l'ensemble de la grille pour faire tomber et donc disparaître les billes du plateau ayant un trou en dessous d'eux
+
+    Args:
+        grille_B (list): Tableau en 2 dimensions représentant la couche des billes
+        grille_H (list): Tableau en 2 dimensions représentant les tirettes horizontaux
+        grille_V (list): Tableau en 2 dimensions représentant les tirettes verticaux
+    """
+    for y in range(len(grille_B)):
+        for x in range(len(grille_B[0])):
+            if grille_H[y][x] + grille_V[y][x] == 0:
+                grille_B[y][x] = False
+
+def victoire(grille_B):
+    """
+    Parcourt l'ensemble de la 
+
+    Args:
+        grille_B (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    for y in range(len(grille_B) - 1):
+        for x in range(len(grille_B[0]) - 1):
+            if grille_B[y][x] == True:
+                return False
+    return True
