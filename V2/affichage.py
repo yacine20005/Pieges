2722b6d3eb8e587cf3f693993977689fdcb17f9d
SkYY6809 <vous.la.connaissez@gmail.com>
2023-12-18 09:27:03 +0100

    Modif

--- V2/affichage.py
+++ V2/affichage.py
@@ -29,21 +29,16 @@ def affiche_plateau():
             rectangle(CDBX,CDBY,CDBX+taille_case,CDBY+taille_case, "black", "white")
 
 def affiche_tirette(plateau):
-    Y = 0
     MFL = L_Fenetre // 2
     MFH = H_Fenetre // 2
     CDBX = MFL - (taille_plateau/2*taille_case) - taille_case
     CDBY = MFH - (taille_plateau/2*taille_case) -taille_case
-    for y in range(taille_plateau):
-        X = 0
-        Y = Y+1
+    for y in range(1, taille_plateau + 1):
         CDBY = CDBY + taille_case
         CDBX = MFL - (taille_plateau/2*taille_case) - taille_case
-        for x in range(taille_plateau):
-            X = X + 1
+        for x in range(1, taille_plateau + 1):
             CDBX = CDBX + taille_case
-            #rectangle(CDBX,CDBY,CDBX+taille_case,CDBY+taille_case, remplissage = etat_tirette(plateau,X,Y), tag = "tirette")
-            cercle(CDBX+taille_case/2,CDBY+taille_case/2, taille_bouton, remplissage = etat_tirette(plateau,X,Y), tag = "tirette")
+            cercle(CDBX+taille_case/2,CDBY+taille_case/2, taille_bouton, remplissage = etat_tirette(plateau,x,y), tag = "tirette")
 
 def affiche_bouton_tirette():
     MFL = L_Fenetre // 2
